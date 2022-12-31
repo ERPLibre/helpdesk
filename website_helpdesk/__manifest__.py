f1b610efcfb68e71ad11c0151624c3bf452d797c
Mathieu Benoit <mathben@technolibre.ca>
2022-12-30 21:32:25 -0500

    [ADD] website_helpdesk: new module - support form with website form builder

---
+++ website_helpdesk/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Helpdesk Form',
+    'category': 'Website',
+    'sequence': 55,
+    'summary': 'Generate ticket from a service form',
+    'version': '2.0',
+    'description': """
+Generate ticket in the Helpdesk app from a form published.
+    """,
+    'depends': [
+        'website_form',
+        'website_partner',
+        'helpdesk_mgmt',
+        'website_form_builder',
+    ],
+    'data': [
+        'data/website_crm_data.xml',
+    ],
+    'qweb': [],
+    'installable': True,
+    'auto_install': True,
+}
